818421fa1cf7a0565bfcb8ead889be286204b489
Determe Stive <stivedeterme@msn.com>
2018-11-02 22:44:50 +0100

    + core.common.php [ add class Common - function Constant, function translate, function MakeConstant ]

--- core/core.common.php
+++ core/core.common.php
@@ -13,3 +13,67 @@ if (!defined('CHECK_INDEX')) {
 	header($_SERVER['SERVER_PROTOCOL'] . ' 403 Direct access forbidden');
 	exit(ERROR_INDEX);
 }
+
+final class Common
+{
+	#########################################
+	# define constant array or simple name
+	#########################################
+	public static function Constant ($data = false, $value = false)
+	{
+		if ($data) {
+			if (is_array($data)) {
+				foreach ($data as $constant => $tableName) {
+					if (!defined(strtoupper($constant))) {
+						$constant = trim($constant);
+						define(strtoupper($constant), $tableName);
+					}
+				}
+			} else {
+				if ($value || $data) {
+					if (!defined(strtoupper($data))) {
+						$data = trim($data);
+						define(strtoupper($data), $value);
+					}
+				}
+			}
+		}
+	}
+	#########################################
+	# return translate if exist
+	#########################################
+	public static function translate ($data, $ucfirst = true) {
+		$str  = $data;
+		$data = self::makeConstant($data);
+		$data = strtoupper($data);
+		if (defined($data)) {
+			$return = $ucfirst === true ? ucfirst(constant($data)) : $str;
+		} else {
+			$return = $ucfirst === true ? ucfirst($str) : $str;
+		}
+		return $return;
+	}
+	#########################################
+	# clear url and constant name
+	#########################################
+	public static function MakeConstant ($d, $c = false) {
+		$chr = array(
+			'À' => 'a', 'Á' => 'a', 'Â' => 'a', 'Ä' => 'a', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ä' => 'a', '@' => 'a',
+			'È' => 'e', 'É' => 'e', 'Ê' => 'e', 'Ë' => 'e', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', '€' => 'e',
+			'Ì' => 'i', 'Í' => 'i', 'Î' => 'i', 'Ï' => 'i', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+			'Ò' => 'o', 'Ó' => 'o', 'Ô' => 'o', 'Ö' => 'o', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'ö' => 'o',
+			'Ù' => 'u', 'Ú' => 'u', 'Û' => 'u', 'Ü' => 'u', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'µ' => 'u',
+			'Œ' => 'oe', 'œ' => 'oe',
+			'$' => 's', '&' => '_AND_', '?' => '%3F');
+		$return = strtr($d, $chr);
+		$return = preg_replace('#[^A-Za-z0-9]+#', '_', $return);
+		$return = trim($return, '-');
+		if ($c == 'upper') {
+			$return = strtoupper($return);
+		} else if ($c == 'lower'){
+			$return = strtolower($return);
+		}
+		return $return;
+	}
+}
+
