7acb547094e5b3e7147f7781cc7440e705d95467
Determe Stive <stivedeterme@msn.com>
2018-11-02 22:47:22 +0100

    + core.notification.php [ add class Notification ]

--- core/core.notification.php
+++ core/core.notification.php
@@ -13,3 +13,32 @@ if (!defined('CHECK_INDEX')) {
 	header($_SERVER['SERVER_PROTOCOL'] . ' 403 Direct access forbidden');
 	exit(ERROR_INDEX);
 }
+
+final class Notification
+{
+	public static function alert($text = NO_TEXT_DEFINED, $title = INFO)
+	{
+		echo self::render(null, $text, $title);
+	}
+	public static function error ($text = NO_TEXT_DEFINED, $title = ERROR)
+	{
+		echo self::render ('error', $text, $title);
+	}
+	public static function warning ($text = NO_TEXT_DEFINED, $title = WARNING)
+	{
+		echo self::render ('warning', $text, $title);
+	}
+	public static function success ($text = NO_TEXT_DEFINED, $title = SUCCESS)
+	{
+		echo self::render ('success', $text, $title);
+	}
+	private static function render ($type = null, $text = 'BEL-CMS : Alert neutral', $title = 'Alert !')
+	{
+		$render  = '<div class="belcms_box_notification '.$type.' ">';
+		$render .= '<strong>'.$title.'</strong>';
+		$render .= $text;
+		$render .= '</div>';
+
+		return $render;
+	}
+}
