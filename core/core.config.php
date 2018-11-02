fab0246b6a8e568c705621243e9d3793db90c779
Determe Stive <stivedeterme@msn.com>
2018-11-02 22:50:42 +0100

    + core.config.php [ add class BelCMSConfig - get config for sql ]

---
+++ core/core.config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Bel-CMS [Content management system]
+ * @version 0.0.1
+ * @link https://bel-cms.be
+ * @link https://stive.eu
+ * @license http://opensource.org/licenses/GPL-3.0 copyleft
+ * @copyright 2014-2019 Bel-CMS
+ * @author Stive - determe@stive.eu
+ */
+
+if (!defined('CHECK_INDEX')) {
+	header($_SERVER['SERVER_PROTOCOL'] . ' 403 Direct access forbidden');
+	exit(ERROR_INDEX);
+}
+
+final class BelCMSConfig
+{
+	function __construct()
+	{
+		$return = array();
+
+		$sql = New BDD;
+		$sql->table('TABLE_CONFIG');
+		$sql->fields(array('name', 'value'));
+		$sql->queryAll();
+		$config = $sql->data;
+		unset($sql);
+		foreach ($config as $k => $v) {
+			$return[mb_strtoupper($v->name)] = (string) $v->value;
+		}
+		Common::Constant($return);
+	}
+}
