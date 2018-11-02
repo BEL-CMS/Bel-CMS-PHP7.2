d20adcfcc2d319053c1a9a19e8ee79a0514cb690
Determe Stive <stivedeterme@msn.com>
2018-11-02 22:42:26 +0100

    + index.php [ insert debug, load page time start, show all request define, insert new belCms + init ]

--- index.php
+++ index.php
@@ -12,17 +12,30 @@
 #########################################
 # start timer
 #########################################
-$start = explode(' ', microtime())[0] + explode(' ', microtime())[1];
+$GLOBALS['TIME_START'] = explode(' ', microtime())[0] + explode(' ', microtime())[1];
 #########################################
 # Initialise TimeZone
 #########################################
 date_default_timezone_set("Europe/Brussels");
 setlocale(LC_TIME, 'fr','fr_FR','fr_FR@euro','fr_FR.utf8','fr-FR','fra');
 #########################################
+# Debug
+#########################################
+function debug ($data = null, $exitAfter = false)
+{
+	echo '<pre>';
+		print_r($data);
+	echo '</pre>';
+	if ($exitAfter === true) {
+		exit();
+	}
+}
+#########################################
 # base constant
 #########################################
 define ('DEBUG', true);
 define ('CHECK_INDEX', true);
+define ('SHOW_ALL_REQUEST_SQL', false);
 define('ERROR_INDEX', '<!doctype html><html lang="fr"><head><meta charset="utf-8"><title>403 Direct access forbidden</title></head><body><h1>Direct access forbidden</h1><p>The requested URL '.$_SERVER['SCRIPT_NAME'].' is prohibited.</p></body></html>');
 define ('DS', '/');
 define ('WEB_ROOT',str_replace('index.php','',$_SERVER['SCRIPT_NAME']));
@@ -31,9 +44,10 @@ define ('ROOT_HTML', 'templates'.DS);
 define ('ROOT_HTML_DFT', 'assets'.DS.'tpl'.DS);
 define ('DIR_CORE', ROOT.'core'.DS);
 define ('DIR_TPL', ROOT.'templates'.DS);
+define ('DIR_TPL_DEFAULT', ROOT.'assets'.DS.'template'.DS);
 define ('DIR_CONFIG', ROOT.'config'.DS);
 define ('DIR_PAGES', ROOT.'pages'.DS);
-define ('DIR_LANG', ROOT.'assets'.DS.'lang'.DS);
+define ('DIR_LANG', ROOT.'assets'.DS.'langs'.DS);
 define ('DIR_ASSET',ROOT.'assets'.DS);
 define ('DIR_ASSET_TPL',ROOT.'assets'.DS.'tpl'.DS);
 define ('DIR_WIDGETS', ROOT.'widgets'.DS);
@@ -41,7 +55,9 @@ define ('DIR_UPLOADS', ROOT.'uploads'.DS);
 #########################################
 # require
 #########################################
-
+require DIR_CORE.'core.error.php';
+require 'file_require.php';
+new RequireFiles;
 #########################################
 # Install
 #########################################
@@ -49,8 +65,10 @@ if (is_file(ROOT.'INSTALL'.DS.'index.php')) {
 	header('Location: INSTALL/index.php');
 	die();
 }
-try {
-	echo('<div>Page generated in '.round((explode(' ', microtime())[0] + explode(' ', microtime())[1]) - $start, 3).' seconds.</div>');
+try {	
+	$BelCMS = new BelCMS;
+	$BelCMS->_init();
+	echo $BelCMS->render;
 } catch (Exception $e) {
 	echo 'Exception reçue : ',  $e->getMessage(), "\n";
 }
