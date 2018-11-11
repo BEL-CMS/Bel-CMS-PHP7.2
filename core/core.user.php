529ae7380dd3a000cfcec1332dfe650c2cd945d9
Determe Stive <stivedeterme@msn.com>
2018-11-11 15:47:52 +0100

    + insert alpha test page user

--- core/core.user.php
+++ core/core.user.php
@@ -17,6 +17,112 @@ class User
 {
 	function __construct ()
 	{
+		debug($_SESSION);
+	}
 
+	public static function isLogged ()
+	{
+		return empty($_SESSION['USER']) ? false : true;
+	}
+	#########################################
+	# Auto connection through cookie
+	#########################################
+	public static function isLogged () {
+		if (isset($_SESSION['USER']) && !empty($_SESSION['USER'])) {
+			$return = (bool) true;
+		} else {
+			unset($_SESSION['USER']);
+			$return = (bool) false;
+		}
+		return $return;
+	}
+	#########################################
+	# Auto connection through cookie
+	#########################################
+	private function autoLogin()
+	{
+		// Si la session existe déjà, inutile d'aller plus loin
+		if (self::isLogged() === false) {
+			// Control si la variable $_COOKIE existe
+			if (isset($_COOKIE['BEL-CMS-COOKIE']) AND !empty($_COOKIE['BEL-CMS-COOKIE'])) {
+				// Passe en tableaux les valeurs du $_COOKIE
+				$cookie = explode('###', $_COOKIE['BEL-CMS-COOKIE']);
+				$name   = $cookie[0]; $hash_key = $cookie[1]; $date = $cookie[2]; $hash = $cookie[3];
+				// Verifie le hash_key est bien de 32 caractere
+				if ($hash_key AND strlen($hash_key) == 32) {
+					self::login($name, $hash, $hash_key);
+				}
+			}
+		}
+	}
+	#########################################
+	# login
+	#########################################
+	public static function login($name = null, $password = null, $hash_key = false)
+	{
+		$sql = New BDD();
+		$sql->table('TABLE_USERS');
+		$sql->isObject(false);
+
+		// Verifie que $name & $password ne son pas vide
+		if (!empty($name) AND !empty($password)) {
+			// Connexion par mail, name ou seulement par hash_key
+			if ($hash_key AND strlen($hash_key) == 32) {
+				$hash_key_search = array(
+					'name'  => 'hash_key',
+					'value' => $hash_key
+				);
+			} else {
+				$hash_key_search = null;
+			}
+			if (strpos($name, '@')) {
+				$request = 'email';
+			} else {
+				$request = 'username';
+			}
+
+			$sql->where(
+				array(
+					'name'  => $request,
+					'value' => $name
+				), $hash_key_search
+			);
+
+			$sql->queryOne();
+
+			$results = $sql->data;
+
+			if ($results && is_array($results) && sizeof($results)) {
+				if ($hash_key AND strlen($hash_key) == 32) {
+					$check_password = $password == $results['password'] ? true : false;
+				} else {
+					$check_password = false;
+				}
+				if (password_verify($password, $results['password']) OR $check_password) {
+					$setcookie = $results['username'].'###'.$results['hash_key'].'###'.date('Y-m-d H:i:s').'###'.$results['password'];
+					setcookie('BEL-CMS-COOKIE', $setcookie, time()+60*60*24*30, '/');
+					$_SESSION['USER'] = self::getInfosUser($results['hash_key']);
+					$return['msg']  = 'La connexion a été éffectuée avec succès';
+					$return['type'] = 'success';
+				} else {
+					$return['msg']  = 'Mauvaise combinaison de Pseudonyme-email et/ou mot de passe';
+					$return['type'] = 'error';
+				}
+			} else {
+				$return['msg']  = 'Aucun utilisateur avec ce Pseudonyme/mail';
+				$return['type'] = 'warning';
+			}
+		} else {
+			$return['msg']  = 'Le nom ou le mot de passe est obligatoire';
+			$return['type'] = 'error';
+		}
+		return $return;
+	}
+	#########################################
+	# Get infos user by id
+	#########################################
+	public static function getInfosUser($hash_key = false, $name = false)
+	{
+		
 	}
 }
